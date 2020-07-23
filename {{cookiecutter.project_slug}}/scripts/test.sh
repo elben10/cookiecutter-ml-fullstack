9c96a4b73ccb7254b2e88e3c4d9ec1f3a8a47ba7
Jakob Jul Elben <jakob@datamaga.com>
2020-07-23 14:57:36 +0200

    Update test script

--- {{cookiecutter.project_slug}}/scripts/test.sh
+++ {{cookiecutter.project_slug}}/scripts/test.sh
@@ -2,3 +2,12 @@
 
 # Exit in case of error
 set -e
+
+docker-compose \
+    -f docker-compose.yml \
+    config > docker-stack.yml
+
+docker-compose -f docker-stack.yml build
+docker-compose -f docker-stack.yml down -v --remove-orphans
+docker-compose -f docker-stack.yml up -d
+docker-compose -f docker-stack.yml down -v --remove-orphans
